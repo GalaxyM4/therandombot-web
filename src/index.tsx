d70f53ac7229610cec61d2dbb82628d6d01aff8e
Luis <luisirigoin123@gmail.com>
2024-04-29 16:21:00 -0500

    React Routes install

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import reportWebVitals from './ts/reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+const router = createBrowserRouter([{
+    path: "/",
+    element: <App />
+}])
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
 
 reportWebVitals();
