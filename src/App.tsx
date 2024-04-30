4e6a34648561f0854ea3e1eb600cdec7f6ed3ee8
Luis <luisirigoin123@gmail.com>
2024-04-30 15:37:49 -0500

    Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import "./css/App.css";
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Trabajando pe
-        </p>
-      </header>
+      <Home />
     </div>
   );
 }
