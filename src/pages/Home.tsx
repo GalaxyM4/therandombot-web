cf86604443e68d6b98db19bd1064d5cc0c681a27
Luis <luisirigoin123@gmail.com>
2024-04-30 15:38:24 -0500

    Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import logo from '../logo.svg';
+
+export default function Home() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Chambiado pe</p>
+    </header>
+  )
+}
