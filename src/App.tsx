06ee907afa87e5516b8a9744080e76b8afa75a20
Luis <luisirigoin123@gmail.com>
2024-05-01 19:15:30 -0500

    NavBar updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./css/App.css";
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Home />
     </div>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,2 +1,29 @@
-import React from "react";
-//vos q
+import { AppBar, Box, Button, SxProps } from "@mui/material";   
+import "../css/Nav.css";
+
+const NavButtonStyle: SxProps = {
+    my: 1.5,
+    mx: 1.5,
+    color: "white",
+    display: "flex",
+    ":hover": {
+        color: "#b0b0b0",
+    }
+}
+
+export default function NavBar() {
+    const pages = ["Inicio 👻", "Comandos 🤓", "Soporte 🐒⚒️"];
+    return (
+        <AppBar position="static" sx={{background: ""}}>
+            <Box className="BoxButtons">
+            {
+                pages.map((page) => (
+                <Button key={page} className="NavButton" sx={NavButtonStyle}>
+                    {page}
+                </Button>
+                ))
+            }
+          </Box>
+        </AppBar>
+    )
+}
