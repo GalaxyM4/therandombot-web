2f7013fc8f235d955144235fae6f1c730228e4c0
Luis <luisirigoin123@gmail.com>
2024-05-01 19:36:06 -0500

    React Routes BUg

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import reportWebVitals from './ts/reportWebVitals';
 import './css/index.css';
-import router from './routes';
-
+import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
