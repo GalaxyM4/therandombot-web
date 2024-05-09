f02f3ed343ee279cc923ba21f0ce3b8e68040f4c
Luis <luisirigoin123@gmail.com>
2024-05-09 12:19:26 -0500

    Image index file

--- src/pages/Chamba.tsx
+++ src/pages/Chamba.tsx
@@ -1,13 +1,9 @@
 import RotateImage from '../components/RotateImage';
-import cow from '../images/animals/cow.png';
-import dodo from '../images/animals/dodo.png';
-import monkey from '../images/animals/monkey.png';
-import orangutan from "../images/animals/orangutan.png";
-import sheep from "../images/animals/sheep.png";
+import ImagesLocal from '../images';
 import { randomItem } from '../ts/utils';
 
 export default function Chamba() {
-  var animals = [monkey, cow, dodo, orangutan, sheep];
+  var animals = ImagesLocal.getAnimalArray();
   return (
     <div className="Chamba-Div">
       <RotateImage image={randomItem(animals)} alt='animal'/>
